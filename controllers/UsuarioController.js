8434f241b206321dd760cbc5954c268f10f41b31
iago bispo dos santos <60926755+iagobispo@users.noreply.github.com>
2021-05-23 01:21:56 -0300

    ...,,,

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -74,8 +74,8 @@ class UsuarioController {
     }
 
     // POST /login
-    login(req, res, next){
-        const { email, password } = req.body;
+   async login(req, res, next){
+        const { email, password } = await req.body;
         Usuario.findOne({ email }).then(async (usuario) => {
             if(!usuario) return res.status(401).json({ errors: "Usuario não registrado" });
             if(!usuario.validarSenha(password)) return res.status(401).json({ errors: "Senha inválida" });
