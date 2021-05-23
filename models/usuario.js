627549d8722554bc54b1635eb8a6591cd4a22585
iago bispo dos santos <60926755+iagobispo@users.noreply.github.com>
2021-05-23 00:52:40 -0300

    ..

--- models/usuario.js
+++ models/usuario.js
@@ -50,10 +50,10 @@ UsuarioSchema.methods.validarSenha = function(password){
     return hash === this.hash;
 };
 
-UsuarioSchema.methods.gerarToken = async function(){
+UsuarioSchema.methods.gerarToken = function(){
     const hoje = new Date();
     const exp = new Date(hoje);
-  await exp.setDate(hoje.getDate() + 15);
+   exp.setDate(hoje.getDate() + 15);
 
     return jwt.sign({
         id: this._id,
